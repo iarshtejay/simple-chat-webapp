107de426380abd0fc0c2c47f13985210ccce94c9
Arsh <arshtejay@gmail.com>
2022-01-15 16:24:47 -0400

    Create a get message service

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require("express")
 var app = express()
 
 app.use(express.static(__dirname))
+
+app.get('/messages', (req,res) => {
+    res.send(messages)
+})
+
+var messages = [
+    {name:'Tim', message:'hello'},
+    {name:'Jim', message:'hey there'}
+]
+
 var server = app.listen(3000, () => {
     console.log('server is listening on port ', server.address().port)
 })
