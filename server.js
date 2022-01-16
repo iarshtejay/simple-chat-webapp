1db54df37e60099e95bb45d76175875999420dda
Arsh <arshtejay@gmail.com>
2022-01-16 19:55:12 -0400

    Create post message service

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require("express")
+var bodyParser = require("body-parser")
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.raw())
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/messages', (req,res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var messages = [
